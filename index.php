9270963931238c2278985bc041d952d54b7505ef
francescodandrea <francescodandrea339@gmail.com>
2022-03-15 18:16:03 +0100

    file init

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require('\pages\landing.php');
+
+?>
---
+++ pages/landing.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PaleoAssenze</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+  </head>
+  <body>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+      PaleoAssenze
+      </h1>
+      <p class="subtitle">
+        On my way
+      </p>
+      <button class="button is-paleo" disabled>Login with PALEOID</button>
+    </div>
+  </section>
+  </body>
+</html>
